60c5617fc5fee0cac8db2c608ece9b150d8490bc
Robert Hendricks <rmasonh@Roberts-MacBook-Pro-4.local>
2024-02-06 01:59:30 -0500

    corrected erro nav works second iteration complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,17 @@ export default function Header() {
     const location = useLocation();
     const navigate = useNavigate();
     const homeClass = pathMatchRoute("/") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent";
+    const offersClass = pathMatchRoute("/offers") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent";
+    const signInClass = pathMatchRoute("/sign-in") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent";
 
     // console.log(location.pathname)
-    // function not working TODO: fix this
+  
     function pathMatchRoute(route) {
         if (route === location.pathname) {
           return true;
         }
-
         return false;
+    
       }
   return (
    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
@@ -28,16 +30,12 @@ export default function Header() {
             <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${homeClass}`} onClick={() => navigate("/")}>
   Home
 </li>
- {/* 
-            <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
-              onClick={() => navigate("/")}>
-              Home
-            </li> */}
-                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}
-                  onClick={()=>navigate("/offers")}>Offers</li>
-                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}
-                  onClick={()=>navigate("/sign-in")}>Sign In</li>
+<li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${offersClass}`} onClick={() => navigate("/offers")}>
+  Offers
+</li>
+<li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${signInClass}`} onClick={() => navigate("/sign-in")}>
+  Sign In
+</li>
             </ul>
         </div>
     </header>
