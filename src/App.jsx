1e5f78e9f9a5b8b18816b1b26dd89471a26900ea
Robert Hendricks <rmasonh@Roberts-MacBook-Pro-4.local>
2024-02-05 22:46:01 -0500

    round edges

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-center text-3xl bg-blue-700 text-white">Welcome to Bujisoft!</h1>
+      <h1 className="text-center text-3xl bg-blue-700 text-white rounded-md">Welcome to Bujisoft!</h1>
     </>
   )
 }
