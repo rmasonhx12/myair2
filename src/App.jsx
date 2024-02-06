58f090ff8ae3477963926faa47943b1e23e97002
Robert Hendricks <rmasonh@Roberts-MacBook-Pro-4.local>
2024-02-05 22:36:07 -0500

    initial project setup for tailwind using vite -- see tailwind documentation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-center text-3xl bg-blue-700 text-white">Welcome to Bujisoft!</h1>
+    </>
+  )
+}
+
+export default App
