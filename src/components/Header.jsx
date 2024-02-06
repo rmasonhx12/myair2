c40d1f9242711c1d486886a5543feca0308d32e4
Robert Hendricks <rmasonh@Roberts-MacBook-Pro-4.local>
2024-02-06 00:37:06 -0500

    Header nav begin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
 //import React from 'react'
+import { useLocation } from "react-router"
 
 export default function Header() {
+    const location = useLocation()
+    console.log(location.pathname)
+    function pathMatchRoute(route) {
+        if(route === location.pathname) {
+            return true
+        }
+    }
   return (
-    <>
-      <h1 className="text-center text-3xl bg-blue-700 text-white rounded-md">Welcome to Bujisoft!</h1>
-    </>
+   <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+            <img src="../../public/cello.png" alt="" 
+            className="h-9"/>    
+        </div>
+        <div>
+            <ul className="flex space-x-10">
+                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`}>Home</li>
+                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}>Offers</li>
+                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}>Sign In</li>
+            </ul>
+        </div>
+    </header>
+   </div>
   )
 }
