91ecc75380373be06d7ea36613120a6e5bad348c
Robert Hendricks <rmasonh@Roberts-MacBook-Pro-4.local>
2024-02-06 01:49:20 -0500

    error with bottom border and  text color change -- update code -- first iteration with error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,43 @@
 //import React from 'react'
-import { useLocation } from "react-router"
+import { useLocation, useNavigate } from "react-router"
 
 export default function Header() {
-    const location = useLocation()
-    console.log(location.pathname)
+    const location = useLocation();
+    const navigate = useNavigate();
+    const homeClass = pathMatchRoute("/") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent";
+
+    // console.log(location.pathname)
+    // function not working TODO: fix this
     function pathMatchRoute(route) {
-        if(route === location.pathname) {
-            return true
+        if (route === location.pathname) {
+          return true;
         }
-    }
+
+        return false;
+      }
   return (
-   <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+   <div className="bg-white border-b shadow-sm sticky top-0 z-40">
     <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
-            <img src="../../public/cello.png" alt="" 
-            className="h-9"/>    
+            <img src="../../public/cello.png" alt="logo" 
+            className="h-9 cursor-pointer"
+            onClick={()=>navigate("/")}/>    
         </div>
         <div>
             <ul className="flex space-x-10">
-                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`}>Home</li>
-                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}>Offers</li>
-                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}>Sign In</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${homeClass}`} onClick={() => navigate("/")}>
+  Home
+</li>
+ {/* 
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`}
+              onClick={() => navigate("/")}>
+              Home
+            </li> */}
+                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`}
+                  onClick={()=>navigate("/offers")}>Offers</li>
+                <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`}
+                  onClick={()=>navigate("/sign-in")}>Sign In</li>
             </ul>
         </div>
     </header>
